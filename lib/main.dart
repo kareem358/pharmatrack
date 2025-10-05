e8edaf9ef45cc20e43515c3100366ac194f3c78f
kareem358 <kareemullahafridi10@gmail.com>
2025-10-06 02:44:18 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,30 +23,3 @@ class PharmaTrackApp extends StatelessWidget {
 
 
 
-/*
-
-import 'package:flutter/material.dart';
-import 'core/config/app_router.dart';
-
-void main() {
-  runApp(const PharmaTrackApp());
-}
-
-class PharmaTrackApp extends StatelessWidget {
-  const PharmaTrackApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'PharmaTrack',
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: AppRouter.generateRoute,
-      initialRoute: AppRouter.login,
-    );
-  }
-}
-
-
-
-
-*/
