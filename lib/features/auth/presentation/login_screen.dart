aeb26964feaf288db61bdbbdbbec395faa609022
kareem358 <kareemullahafridi10@gmail.com>
2025-10-08 02:21:57 +0500

    Update login_screen.dart

--- lib/features/auth/presentation/login_screen.dart
+++ lib/features/auth/presentation/login_screen.dart
@@ -53,27 +53,3 @@ class LoginScreen extends StatelessWidget {
 }
 
 
-
-/*
-import 'package:flutter/material.dart';
-import 'package:pharmatrack/core/config/app_router.dart';
-
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, AppRouter.dashboard);
-          },
-          child: const Text('Go to Dashboard'),
-        ),
-      ),
-    );
-  }
-}
-*/
