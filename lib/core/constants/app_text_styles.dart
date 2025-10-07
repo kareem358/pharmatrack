5b77da887a568039d10f1a24c47c82df2dea20dc
kareem358 <kareemullahafridi10@gmail.com>
2025-10-08 01:55:33 +0500

    Update app_text_styles.dart

--- lib/core/constants/app_text_styles.dart
+++ lib/core/constants/app_text_styles.dart
@@ -46,34 +46,3 @@ class AppTextStyles {
   );
 }
 
-
-
-
-/*
-import 'package:flutter/material.dart';
-
-class AppTextStyles {
-  static const TextStyle heading = TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.bold,
-    color: Colors.black87,
-  );
-
-  static const TextStyle body = TextStyle(
-    fontSize: 16,
-    color: Colors.black87,
-  );
-
-  static const TextStyle label = TextStyle(
-    fontSize: 14,
-    fontWeight: FontWeight.w500,
-    color: Colors.black54,
-  );
-
-  static TextTheme get textTheme => const TextTheme(
-    titleLarge: heading,
-    bodyMedium: body,
-    labelMedium: label,
-  );
-}
-*/
