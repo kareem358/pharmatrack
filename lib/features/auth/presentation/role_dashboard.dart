d24cc4a0f37ae46363cd7d9a4ee2fe439108c93d
kareem358 <kareemullahafridi10@gmail.com>
2025-10-08 02:48:23 +0500

    Update role_dashboard.dart

--- lib/features/auth/presentation/role_dashboard.dart
+++ lib/features/auth/presentation/role_dashboard.dart
@@ -52,33 +52,3 @@ class RoleDashboard extends StatelessWidget {
   }
 }
 
-
-/*
-import 'package:flutter/material.dart';
-
-class RoleDashboard extends StatelessWidget {
-  const RoleDashboard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Role Dashboard')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Welcome to Role Dashboard'),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/inventory');
-              },
-              child: const Text('Go to Inventory'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-*/
